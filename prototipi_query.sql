3861918d13e5d8b7e8646ceb13f5a650ff8e2f09
Alessandro Benetton <aleben98@gmail.com>
2022-08-20 17:28:12 +0200

    Prototipo query e credenziali

---
+++ prototipi_query.sql
@@ -0,0 +1,34 @@
+-- Mobile Query
+
+-- Richiesta POI - IN: category, minRank, pos (order by dist)
+SELECT poi.id, poi.name, poi.rank, cat.name, st_asgeojson(poi.geom) as coordinates, st_distance(poi.geom,st_geogfromtext('POINT(11.3428 44.4939)')) as meters
+FROM "black-kiwi_data".poi_list as poi
+JOIN "black-kiwi_data".categories as cat on poi.category = cat.id
+WHERE cat.name = 'park' and poi.rank<0
+ORDER BY meters;
+
+-- Admin query
+-- Visualizzare POI disponibili
+SELECT poi.id, poi.name, poi.rank, cat.name, st_asgeojson(poi.geom) as coordinates
+FROM "black-kiwi_data".poi_list as poi
+JOIN "black-kiwi_data".categories as cat on poi.category = cat.id;
+
+-- Add a new POI
+-- TODO
+
+-- Visualizzare posizione utenti
+SELECT *
+FROM "black-kiwi_data".requests;
+
+-- Visualizzare quartieri colorati su base di poi in esso contenuti
+SELECT quartieri.nomequart, quartieri.geom, count(poi.name) as poi_density
+FROM "black-kiwi_data".poi_list as poi
+JOIN "black-kiwi_data"."quartieri-bologna" as quartieri on st_within(poi.geom, quartieri.geom)
+GROUP BY quartieri.nomequart, quartieri.geom;
+
+-- Visualizzare quartieri colorati su base di checkin utenti
+-- TODO
+
+
+-- Clustering spaziale
+-- TODO
