cb2700dd245de94ebe4682d81a9c7cbc568158b3
Alessandro Benetton <aleben98@gmail.com>
2022-11-09 18:58:00 +0100

    Propagate CORS headers to all admin-available endpoints

--- src/endpoints/admin/getRequestLocations.go
+++ src/endpoints/admin/getRequestLocations.go
@@ -11,5 +11,10 @@ import (
 
 // getAlbums responds with the list of all albums as JSON.
 func GetRequestLocations(c *gin.Context) {
+
+	// Allow CORS
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	
 	c.IndentedJSON(http.StatusOK, black_kiwi_data_structs.MockRequestInfo)
 }
--- src/endpoints/default/postLogin.go
+++ src/endpoints/default/postLogin.go
@@ -14,6 +14,10 @@ import (
 
 func PostLogin(c *gin.Context) {
 
+	// Allow CORS
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	username := c.PostForm("username")
 	password := c.PostForm("password")
 
@@ -22,10 +26,6 @@ func PostLogin(c *gin.Context) {
 		"username": username,
 		"password": password,
 		}).Info("Endpoint called")
-
-	// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	if os.Getenv("Black_Kiwi_ENV") == "dev-nodb" {
 		log.WithFields(log.Fields{"endpoint": "PostLogin"}).Info("Endpoint called in dev-nodb mode")
