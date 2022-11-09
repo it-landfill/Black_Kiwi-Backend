f5fca102d4aefad348df3f9aa06d6ea1655a5e9f
Alessandro Benetton <aleben98@gmail.com>
2022-11-09 18:54:52 +0100

    Allow CORS from all sources
    Might be removed later

--- src/main.go
+++ src/main.go
@@ -38,7 +38,7 @@ func main() {
     router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:8080"},
+		AllowOrigins: []string{"*"},
 	}))
 
 	// Default
