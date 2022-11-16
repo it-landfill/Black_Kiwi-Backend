62cd7181e9cd1c634e3b45647d2856963ce09430
Alessandro Benetton <aleben98@gmail.com>
2022-11-16 13:51:26 +0100

    Implemented getPOIs query

---
+++ src/dbHandler/default/getPOISQuery.go
@@ -0,0 +1,77 @@
+package black_kiwi_login_queries
+
+import (
+	"context"
+
+	log "github.com/sirupsen/logrus"
+
+	black_kiwi_db_utils "ITLandfill/Black-Kiwi/dbHandler/utils"
+	"ITLandfill/Black-Kiwi/structs/data_structs"
+)
+
+//https://app.swaggerhub.com/apis/ITLandfill/Black-Kiwi/1.0.2
+
+/*
+SELECT poi.id, poi.name, poi.rank, cat.name, st_asgeojson(poi.geom) as coordinates
+FROM "black-kiwi_data".poi_list as poi
+JOIN "black-kiwi_data".categories as cat on poi.category = cat.id
+*/
+func GetPOIS() (result bool, poiList *[]black_kiwi_data_structs.PoiItem)  {
+
+	rows, err := black_kiwi_db_utils.ConnPool.Query(context.Background(), "SELECT poi.id, poi.name, poi.rank, cat.name, st_asgeojson(poi.geom) as coordinates FROM \"black-kiwi_data\".poi_list as poi JOIN \"black-kiwi_data\".categories as cat on poi.category = cat.id")
+	if err != nil {
+		log.WithFields(log.Fields{"error":err}).Error("QueryRow failed for get POIS.")
+		return false, nil
+	}
+	defer rows.Close()
+
+	tmpList := []black_kiwi_data_structs.PoiItem{}
+
+	for rows.Next() {
+		var id int
+		var name string
+		var rank float32
+		var category string
+		var coordinates string
+		err = rows.Scan(&id, &name, &rank, &category, &coordinates)
+		if err != nil {
+			if (err.Error() == "no rows in result set") {
+				log.Info("No POIs found in db.")
+				return true, nil
+			}
+
+			log.WithFields(log.Fields{"error":err}).Error("QueryRow failed while scanning rows for get POIS.")
+			return false, nil
+		}
+
+		var cat black_kiwi_data_structs.Categories
+		switch category {
+		case "Park":
+			cat = black_kiwi_data_structs.PARK
+		case "Museum":
+			cat = black_kiwi_data_structs.MUSEUM
+		case "Historical Building":
+			cat = black_kiwi_data_structs.HISTORICAL_BUILDING
+		case "Theater":
+			cat = black_kiwi_data_structs.THEATER
+		case "Department":
+			cat = black_kiwi_data_structs.DEPARTMENT
+		}
+
+		tmpLon, tmpLat := black_kiwi_db_utils.JSONtoCoordinates(coordinates)
+
+		poiItem := black_kiwi_data_structs.PoiItem{
+			Id:       id,
+			Name:     name,
+			Rank:     rank,
+			Category: cat,
+			Coord: black_kiwi_data_structs.Coordinates{
+				Latitude:  tmpLat,
+				Longitude: tmpLon,
+			},
+		}
+
+		tmpList = append(tmpList, poiItem)
+	}
+	return true, &tmpList
+}
