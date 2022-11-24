90532684aabc9aaae206fba931d676ee9888fda8
Alessandro Benetton <aleben98@gmail.com>
2022-11-24 20:29:59 +0100

    Implemented getCheckinQuartieri

--- src/endpoints/admin/getCheckinQuartieri.go
+++ src/endpoints/admin/getCheckinQuartieri.go
@@ -2,17 +2,41 @@ package black_kiwi_admin
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 
 	log "github.com/sirupsen/logrus"
 
 	"ITLandfill/Black-Kiwi/structs/data_structs"
+	"ITLandfill/Black-Kiwi/dbHandler/admin"
 )
 
 // getAlbums responds with the list of all albums as JSON.
 func GetCheckinQuartieri(c *gin.Context) {
 	log.Info("GetCheckinQuartieri endpoint called")
 	
-	c.IndentedJSON(http.StatusOK, black_kiwi_data_structs.MockQuartiereInfo)
+	var featureCollection string
+	var result bool
+
+	if os.Getenv("Black_Kiwi_ENV") == "dev-nodb" {
+		log.WithFields(log.Fields{"endpoint": "GetCheckinQuartieri"}).Info("Endpoint called in dev-nodb mode")
+		featureCollection = black_kiwi_data_structs.MockQuartiereInfo
+		result = true
+	} else {
+		result, featureCollection = black_kiwi_admin_queries.GetCheckinQuartieri()
+	}
+
+	if !result {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while getting quartieri info"})
+		return
+	}
+
+	if featureCollection == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No quartieri found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, featureCollection)
 }
+	
--- src/main.go
+++ src/main.go
@@ -19,6 +19,10 @@ import (
 	"ITLandfill/Black-Kiwi/dbHandler/utils"
 )
 
+/*
+TODO: Implement queries for getRequestLocation and getCheckinQuartieri
+*/
+
 func main() {
 
 	if os.Getenv("Black_Kiwi_ENV") != "" {
