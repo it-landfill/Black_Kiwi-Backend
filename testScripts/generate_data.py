217692fe00f01a04157a7e4d05d835d03d3fd37e
Alessandro Benetton <aleben98@gmail.com>
2022-12-12 10:33:41 +0100

    Update dummies generation method

--- testScripts/generate_data.py
+++ testScripts/generate_data.py
@@ -95,40 +95,34 @@ def var_radius_data(n, min, max, divider):
     qos_df = pd.DataFrame(qos)
     utils.save_df("data/var_radius_qos.csv", qos_df)
 
-def var_dummies_data(n, radius):
+def var_dummies_data(n, dummies, radius):
     center = [11.343083, 44.494332]
 
     pp = []
 
-    uni = generate_pos(n, center, radius, genProbStat.generateUniformNoise, None, True)
-    poi = generate_pos(n, center, radius, poisson, None, True)
-    gau = generate_pos(n, center, radius, gaussian, None, True)
-    tri = generate_pos(n, center, radius, triangular, None, True)
+    pbar = tqdm(range(n))
+    for _ in pbar:
+        for dummies_num in range(2, dummies+1):
+            pbar.set_description(f"Generating data for {dummies_num} dummies")
+            uni = generate_pos(dummies_num, center, radius, genProbStat.generateUniformNoise, None, True)
+            poi = generate_pos(dummies_num, center, radius, poisson, None, True)
+            gau = generate_pos(dummies_num, center, radius, gaussian, None, True)
+            tri = generate_pos(dummies_num, center, radius, triangular, None, True)
 
-    pbar = tqdm(range(1, n+1))
-    for dummies in pbar:
-        uni_centroid = []
-        poi_centroid = []
-        gau_centroid = []
-        tri_centroid = []
+            uni_centroid = genProbStat.centroidCalculationAll(uni)
+            poi_centroid = genProbStat.centroidCalculationAll(poi)
+            gau_centroid = genProbStat.centroidCalculationAll(gau)
+            tri_centroid = genProbStat.centroidCalculationAll(tri)
 
-        for i in range(0, n-dummies+1):
-            pbar.set_description(f"Calculating dummies. Window size {i+dummies}")
-            uni_centroid.append(genProbStat.centroidCalculationAll(uni[i:i+dummies]))
-            poi_centroid.append(genProbStat.centroidCalculationAll(poi[i:i+dummies]))
-            gau_centroid.append(genProbStat.centroidCalculationAll(gau[i:i+dummies]))
-            tri_centroid.append(genProbStat.centroidCalculationAll(tri[i:i+dummies]))
+            uni_pp = utils.get_distance(center, uni_centroid)
+            poi_pp = utils.get_distance(center, poi_centroid)
+            gau_pp = utils.get_distance(center, gau_centroid)
+            tri_pp = utils.get_distance(center, tri_centroid)
 
-        uni_pp = utils.privacy_preservation_arr(center, uni_centroid)
-        poi_pp = utils.privacy_preservation_arr(center, poi_centroid)
-        gau_pp = utils.privacy_preservation_arr(center, gau_centroid)
-        tri_pp = utils.privacy_preservation_arr(center, tri_centroid)
-
-        for i in range(len(uni_pp)):
-            pp.append({"dummies": dummies, "value": uni_pp[i], "type": "uniform"})
-            pp.append({"dummies": dummies, "value": poi_pp[i], "type": "poisson"})
-            pp.append({"dummies": dummies, "value": gau_pp[i], "type": "gaussian"})
-            pp.append({"dummies": dummies, "value": tri_pp[i], "type": "triangular"})
+            pp.append({"dummies": dummies_num, "value": uni_pp, "type": "uniform"})
+            pp.append({"dummies": dummies_num, "value": poi_pp, "type": "poisson"})
+            pp.append({"dummies": dummies_num, "value": gau_pp, "type": "gaussian"})
+            pp.append({"dummies": dummies_num, "value": tri_pp, "type": "triangular"})
 
     pp_df = pd.DataFrame(pp)
     utils.save_df("data/var_dummies_pp.csv", pp_df)
@@ -141,4 +135,4 @@ if __name__ == "__main__":
     #generate_fake_pos("data/true_pos.json", 4, gaussian, "km4/fake_gaussian.json")
     #generate_fake_pos("data/true_pos.json", 4, triangular, "km4/fake_triangular.json")
     #var_radius_data(100, 0, 4, 4)
-    var_dummies_data(1000, 2)
+    var_dummies_data(n=200, dummies=1000, radius=2)
